562c213d14b1f4fbbfd2a9e8f0fb3db8cbdf4ea6
mdinoshc <mdinoshc@gmail.com>
2022-01-25 08:22:40 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer() {
+    return <div style={{backgroundColor: 'blue'}}>
+        <p style={{color:'white'}}>Designed and Developed by</p>
+        <p style={{color:'white'}}>Edubyte Calculus</p>
+    </div>
+}
+
+export default Footer
---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Navbar() {
+    return <div style={{backgroundColor: 'brown'}}>
+        <h1>React meals</h1>
+        <a style={{margin: '10px', color: 'white'}}>Home</a>
+        <a style={{margin: '10px', color: 'white'}}>About</a>
+        <a style={{margin: '10px', color: 'white'}}>Contact us</a>
+    </div>
+}
+
+export default Navbar
